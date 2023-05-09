7a6314dd1732dc19be7be7ec2e352a822b519d6b
Jakkor <JakkAeson@users.noreply.github.com>
2023-05-09 10:35:47 +0800

    Add project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace ConvolutionFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox02 = new System.Windows.Forms.TextBox();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.textBox01 = new System.Windows.Forms.TextBox();
+            this.textBox00 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox22 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(680, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(686, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kernel :";
+            // 
+            // textBox02
+            // 
+            this.textBox02.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox02.Location = new System.Drawing.Point(758, 27);
+            this.textBox02.MaxLength = 4;
+            this.textBox02.Name = "textBox02";
+            this.textBox02.Size = new System.Drawing.Size(30, 23);
+            this.textBox02.TabIndex = 4;
+            this.textBox02.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox02.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox02.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonApply.Location = new System.Drawing.Point(686, 114);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(102, 23);
+            this.buttonApply.TabIndex = 11;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // textBox01
+            // 
+            this.textBox01.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox01.Location = new System.Drawing.Point(722, 27);
+            this.textBox01.MaxLength = 4;
+            this.textBox01.Name = "textBox01";
+            this.textBox01.Size = new System.Drawing.Size(30, 23);
+            this.textBox01.TabIndex = 3;
+            this.textBox01.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox01.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox01.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // textBox00
+            // 
+            this.textBox00.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox00.Location = new System.Drawing.Point(686, 27);
+            this.textBox00.MaxLength = 4;
+            this.textBox00.Name = "textBox00";
+            this.textBox00.Size = new System.Drawing.Size(30, 23);
+            this.textBox00.TabIndex = 2;
+            this.textBox00.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox00.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox00.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox12.Location = new System.Drawing.Point(758, 56);
+            this.textBox12.MaxLength = 4;
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(30, 23);
+            this.textBox12.TabIndex = 7;
+            this.textBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox12.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox12.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox11.Location = new System.Drawing.Point(722, 56);
+            this.textBox11.MaxLength = 4;
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(30, 23);
+            this.textBox11.TabIndex = 6;
+            this.textBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox11.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox11.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox10.Location = new System.Drawing.Point(686, 56);
+            this.textBox10.MaxLength = 4;
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(30, 23);
+            this.textBox10.TabIndex = 5;
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox10.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox10.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // textBox22
+            // 
+            this.textBox22.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox22.Location = new System.Drawing.Point(758, 85);
+            this.textBox22.MaxLength = 4;
+            this.textBox22.Name = "textBox22";
+            this.textBox22.Size = new System.Drawing.Size(30, 23);
+            this.textBox22.TabIndex = 10;
+            this.textBox22.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox22.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox22.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // textBox21
+            // 
+            this.textBox21.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox21.Location = new System.Drawing.Point(722, 85);
+            this.textBox21.MaxLength = 4;
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(30, 23);
+            this.textBox21.TabIndex = 9;
+            this.textBox21.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox21.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox21.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // textBox20
+            // 
+            this.textBox20.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox20.Location = new System.Drawing.Point(686, 85);
+            this.textBox20.MaxLength = 4;
+            this.textBox20.Name = "textBox20";
+            this.textBox20.Size = new System.Drawing.Size(30, 23);
+            this.textBox20.TabIndex = 8;
+            this.textBox20.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox20.Enter += new System.EventHandler(this.OnTextBoxSelect);
+            this.textBox20.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnTextBoxMouseUp);
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowse.Location = new System.Drawing.Point(686, 143);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(102, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Browse...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // labelTime
+            // 
+            this.labelTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(686, 173);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(29, 15);
+            this.labelTime.TabIndex = 12;
+            this.labelTime.Text = "0ms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox20);
+            this.Controls.Add(this.textBox21);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox22);
+            this.Controls.Add(this.textBox00);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox01);
+            this.Controls.Add(this.textBox02);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Convolution Filter";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox02;
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.TextBox textBox01;
+        private System.Windows.Forms.TextBox textBox00;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox22;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.TextBox textBox20;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvolutionFilter
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap sourceImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new()
+            {
+                Filter = "Image files (*.jpg; *.png; *.bmp)|*.jpg; *.png; *.bmp"
+            };
+
+            if (fileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            sourceImage?.Dispose();
+            sourceImage = new Bitmap(fileDialog.FileName);
+
+            fileDialog.Dispose();
+
+            if (sourceImage.Width > panel1.Width || sourceImage.Height > panel1.Height)
+            {
+                panel1.BackgroundImageLayout = ImageLayout.Zoom;
+            }
+            else
+            {
+                panel1.BackgroundImageLayout = ImageLayout.Center;
+            }
+
+            panel1.BackgroundImage?.Dispose();
+            panel1.BackgroundImage = sourceImage;
+            panel1.Refresh();
+        }
+
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            if (sourceImage == null)
+            {
+                return;
+            }
+
+            Cursor.Current = Cursors.WaitCursor;
+
+            int[][] kernel = new int[][] { new int[3], new int[3], new int[3] };
+            try
+            {
+                kernel[0][0] = int.Parse(textBox00.Text);
+                kernel[0][1] = int.Parse(textBox01.Text);
+                kernel[0][2] = int.Parse(textBox02.Text);
+
+                kernel[1][0] = int.Parse(textBox10.Text);
+                kernel[1][1] = int.Parse(textBox11.Text);
+                kernel[1][2] = int.Parse(textBox12.Text);
+
+                kernel[2][0] = int.Parse(textBox20.Text);
+                kernel[2][1] = int.Parse(textBox21.Text);
+                kernel[2][2] = int.Parse(textBox22.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            double kernelSum = 0;
+            foreach (int[] row in kernel)
+            {
+                kernelSum += row.Sum();
+            }
+            kernelSum = (kernelSum == 0) ? 1 : kernelSum;
+
+            Bitmap resultImage = new(sourceImage.Width, sourceImage.Height, sourceImage.PixelFormat);
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            //for (int i = 0; i < sourceImage.Width; i++)
+            //{
+            //    for (int j = 0; j < sourceImage.Height; j++)
+            //    {
+            //        resultImage.SetPixel(i, j, ConvolutePixel(sourceImage, i, j, kernel, kernelSum));
+            //    }
+            //}
+
+            ParallelConvolute(sourceImage, resultImage, kernel, kernelSum);
+
+            stopwatch.Stop();
+
+            if (panel1.BackgroundImage != sourceImage && panel1.BackgroundImage != null)
+            {
+                panel1.BackgroundImage.Dispose();
+            }
+            panel1.BackgroundImage = resultImage;
+            panel1.Refresh();
+
+            Cursor.Current = Cursors.Default;
+
+            labelTime.Text = $"{stopwatch.ElapsedMilliseconds}ms";
+        }
+
+        private static Color ConvolutePixel(Bitmap image, int x, int y, int[][] kernel, double kernelSum)
+        {
+            double r = 0;
+            double g = 0;
+            double b = 0;
+
+            int edgeX = (x > 0 && x < image.Width - 1) ? 1 : 0;
+            int edgeY = (y > 0 && y < image.Height - 1) ? 1 : 0;
+
+            for (int i = 0; i < kernel.Length; i++)
+            {
+                y += edgeY * (i - 1);
+
+                for (int j = 0; j < kernel[i].Length; j++)
+                {
+                    x += edgeX * (j - 1);
+
+                    r += image.GetPixel(x, y).R * kernel[i][j];
+                    g += image.GetPixel(x, y).G * kernel[i][j];
+                    b += image.GetPixel(x, y).B * kernel[i][j];
+                }
+            }
+
+            r = Math.Round(Math.Clamp(r / kernelSum, 0, 255));
+            g = Math.Round(Math.Clamp(g / kernelSum, 0, 255));
+            b = Math.Round(Math.Clamp(b / kernelSum, 0, 255));
+
+            return Color.FromArgb((int)r, (int)g, (int)b);
+        }
+
+        private static unsafe void ParallelConvolute(Bitmap image, Bitmap result, int[][] kernel, double kernelSum)
+        {
+            Rectangle rect = new(0, 0, image.Width, image.Height);
+
+            BitmapData imageData = image.LockBits(rect, ImageLockMode.ReadOnly, image.PixelFormat);
+            BitmapData resultData = result.LockBits(rect, ImageLockMode.WriteOnly, result.PixelFormat);
+
+            byte* imageStartPtr = (byte*)imageData.Scan0;
+            byte* resultStartPtr = (byte*)resultData.Scan0;
+
+            int bytesPerPixel = Image.GetPixelFormatSize(image.PixelFormat) / 8;    // 8 bits in a byte
+            int widthBytes = image.Width * bytesPerPixel;
+            int height = image.Height;
+            int stride = imageData.Stride;
+
+            Parallel.For(0, height, y =>
+            {
+                byte* imageCurrentLine = imageStartPtr + (y * stride);
+                byte* resultCurrentLine = resultStartPtr + (y * stride);
+                
+                int edgeY = (y > 0 && y < height - 1) ? 1 : 0;
+
+                for (int x = 0; x < widthBytes; x += bytesPerPixel)
+                {
+                    int edgeX = (x > 0 && x < widthBytes - bytesPerPixel) ? 1 : 0;
+
+                    double r = 0;
+                    double g = 0;
+                    double b = 0;
+
+                    for (int i = 0; i < kernel.Length; i++)
+                    {
+                        imageCurrentLine += edgeY * (i - 1) * stride;
+
+                        for (int j = 0; j < kernel[i].Length; j++)
+                        {
+                            x += edgeX * (j - 1) * bytesPerPixel;
+
+                            r += imageCurrentLine[x + 2] * kernel[i][j];
+                            g += imageCurrentLine[x + 1] * kernel[i][j];
+                            b += imageCurrentLine[x] * kernel[i][j];
+                        }
+                    }
+
+                    resultCurrentLine[x + 2] = (byte)Math.Round(Math.Clamp(r / kernelSum, 0, 255));
+                    resultCurrentLine[x + 1] = (byte)Math.Round(Math.Clamp(g / kernelSum, 0, 255));
+                    resultCurrentLine[x] = (byte)Math.Round(Math.Clamp(b / kernelSum, 0, 255));
+                }
+            });
+
+            image.UnlockBits(imageData);
+            result.UnlockBits(resultData);
+        }
+
+        private void OnTextBoxSelect(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.SelectAll();
+        }
+
+        private void OnTextBoxMouseUp(object sender, MouseEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.SelectAll();
+        }
+    }
+}
